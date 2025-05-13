da94ee5b38bbe86d7aebb9d9c9581b9562e09d9f
Lawson C <lawson.conallin@gmail.com>
2025-05-13 11:49:36 +1000

    allow a testing server to run

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 # Main Server File
 import os, sys, time, json, datetime
 from flask import Flask, request, jsonify, render_template, redirect, send_file, make_response
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+app.run(port=5000)
