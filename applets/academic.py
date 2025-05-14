7eb642265d51b3c59b2966d694b84a17f3658864
Lawson C <lawson.conallin@gmail.com>
2025-05-15 08:51:34 +1000

    dumbass

--- applets/academic.py
+++ applets/academic.py
@@ -6,7 +6,7 @@ base = "academic"
 
 
 # Applet
-from flask import Blueprint, render_template, abort
+from flask_example import Blueprint, render_template, abort
 from jinja2 import TemplateNotFound
 from __main__ import core
 
