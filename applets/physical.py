ff947738c288f382324c35eb5d71873de4010a5f
NotLawson <lawson.conallin@gmail.com>
2025-06-19 18:26:33 +1000

    small fixes
    Co-authored-by: Mitchell Howard <mitchellxhoward@gmail.com>

--- applets/physical.py
+++ applets/physical.py
@@ -1,9 +1,10 @@
 # Physical Applet
 # flask is pretty important for a flask applet
+from flask import render_template
 from __main__ import core
 
 # applet function
 def physical_main():
-    return 'it works kinda'
+    return render_template('/physical.html')
 
 core.register_page(physical_main, '/physical')
